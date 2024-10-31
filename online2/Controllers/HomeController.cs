08a730778451a5e839cc186af7ccb4ca6fe2eec3
DELL <DELL@DESKTOP-ET451QD>
2024-10-31 15:58:37 +0700

    Add project files.

---
+++ online2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace online2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult text()
+        {
+            return View();
+        }
+//@* 23DH111859_DinhThiTrucLinh*@
+    }
+}
